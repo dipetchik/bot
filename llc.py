589c765974faae865252976bc9205dde332c513b
dipetchik <kozmenkovlad10@gmail.com>
2026-03-01 16:01:56 +0300

    Update llc.py

--- llc.py
+++ llc.py
@@ -6,8 +6,8 @@ import hashlib
 from datetime import datetime
 from aiogram import Bot, Dispatcher, types
 from aiogram.fsm.storage.memory import MemoryStorage
-from aiogram.dispatcher import FSMContext
-from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.filters.state import State, StatesGroup
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils.exceptions import MessageNotModified
 
@@ -738,4 +738,5 @@ if __name__ == '__main__':
     print("=" * 50)
 
     executor.start_polling(dp, skip_updates=True)
+
 
